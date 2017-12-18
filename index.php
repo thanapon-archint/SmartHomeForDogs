a4314029bafebd37cab49a263a038a337f56930d
Pun <pannathornn@gmail.com>
2017-12-18 16:17:26 +0700

    eiei

--- index.php
+++ index.php
@@ -14,9 +14,26 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 </style>
 <body>
 
-<?php
-require_once('Navbar.php');
-?>
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Login</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Dogs</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">DOG LIST</a>
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Search</a>
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Login</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Dogs</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">DOG LIST</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">Search</a>
+  </div>
+</div>
+
 <!-- Header -->
 <header class="w3-container w3-red w3-center" style="padding:128px 16px">
   <h1 class="w3-margin w3-jumbo">SMART HOME FOR DOGS</h1>
@@ -60,7 +77,6 @@ require_once('Navbar.php');
   </div>
 </div>
 
-<<<<<<< HEAD
 <style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
@@ -97,27 +113,6 @@ require_once('Navbar.php');
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
-=======
-<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">
-      <!-- Google Map -->
-          <div id="googleMap" class="w3-grayscale" style="width:100%;height:450px;"></div>
-
-          <!-- Footer -->
-          <footer class="w3-center w3-black w3-padding-16">
-            <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-          </footer>
-
-          <!-- Add Google Maps -->
-          <script>
-          function myMap()
-          {
-            myCenter=new google.maps.LatLng(41.878114, -87.629798);
-            var mapOptions= {
-              center:myCenter,
-              zoom:12, scrollwheel: true, draggable: true,
-              mapTypeId:google.maps.MapTypeId.ROADMAP
->>>>>>> 907d39c8896b13fbe5e2cc66e81295e86e02fe91
             };
 
             infoWindow.setPosition(pos);
