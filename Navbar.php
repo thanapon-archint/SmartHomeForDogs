27da57014819d93dd79bd407ff55c62466a50e52
Chayaphat <xjellomasterx@gmail.com>
2017-12-20 13:26:33 +0700

    ใส่ภาพหมาหน้าโง่

--- Navbar.php
+++ Navbar.php
@@ -3,7 +3,7 @@
   <div class="w3-bar w3-red w3-card w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Login</a>
+    <a href="login-form.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Login</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Dogs</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">DOG LIST</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Search</a>
@@ -11,7 +11,7 @@
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">Login</a>
+    <a href="login-form.php" class="w3-bar-item w3-button w3-padding-large">Login</a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">Dogs</a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">DOG LIST</a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">Search</a>
--- index.php
+++ index.php
@@ -8,9 +8,12 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+body,h1,h2,h3,h4,h6 {font-family: "Lato", sans-serif}
 .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
 .fa-anchor,.fa-coffee {font-size:200px}
+h5{
+  font-family: 'Designil Font', 'Helvetica', sans-serif;
+}
 </style>
 <body>
 
@@ -28,16 +31,19 @@ require_once('Navbar.php');
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-twothird">
-      <h1>Lorem Ipsum</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+      <h1>บ้านสุนัขอัจฉริยะแสนสุข</h1>
+      <h5 class="w3-padding-32" >ปัจจุบันนี้ มีสุนัขที่ถูกทอดทิ้งเพิ่มขึ้นเป็นจำนวนมาก ทำให้มีสุนัขจรจัดเพิ่มขึ้นอย่างต่อเนื่อง ส่งผลให้มีจำนวนสุนัขภายในมูลนิธิช่วยเหลือสัตว์เลี้ยงมีจำนวนเพิ่มขึ้น ซึ่งจัดเป็นปัญหาเรื้อรังที่ส่งผลต่อการจัดสรรงบประมาณ การวางแนวทางการดูแลสุนัข และการเร่งทำหมันสุนัขภายในมูลนิธิ ประกอบกับหลายครัวเรือนประสบปัญหาการเลี้ยงดูสุนัข เช่น การไม่พร้อมดูแล การเคลื่อนย้ายสัตว์เลี้ยงในขณะที่กำลังย้ายที่อยู่อาศัย หรือการขาดความรับผิดชอบต่อสุนัขที่เลี้ยง 
+จึงได้มีการจัดทำเว็บไซต์เพื่อประชาสัมพันธ์ให้มีการป้องกันและแก้ไขปัญหาดังกล่าวโดยการเปิดช่องทางออนไลน์ในการหาที่อยู่อาศัยให้สุนัขและส่งเสริมให้มีการเลี้ยงสุนัขโดยไม่มีความจำเป็นต้องเกิดการซื้อขาย</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <img src="Images/groupofdoggy.JPEG" width="700" height="200">
     </div>
 
     <div class="w3-third w3-center">
-      <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
+      <i class="w3-padding-64 w3-text-red">
+        <img src="Images/doggy.jpg" width= "320" height="220" align="left">
+
+      </i>
+
     </div>
   </div>
 </div>
