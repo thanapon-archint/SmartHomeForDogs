80cb6f954ce4153ef438c75c5adcc7a04ad99832
Pun <pannathornn@gmail.com>
2017-12-21 13:02:47 +0700

    Add recenter button

--- map.php
+++ map.php
@@ -36,6 +36,14 @@
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
+            // Create the DIV to hold the control and call the CenterControl()
+            // constructor passing in this DIV.
+            var centerControlDiv = document.createElement('div');
+            var centerControl = new CenterControl(centerControlDiv, map);
+
+            centerControlDiv.index = 1;
+            map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+          
 
             infoWindow.setPosition(pos);
             infoWindow.setContent("You're here.");
@@ -68,19 +76,46 @@
             position: {lat:14.2123158585, lng:101.23579999999},
             map: map,
             icon:homeicons
-            url: "dog_list.php"
             });
             marker.addListener('click', function() {
-            // linked to info view of the dog.
-
-            map.setZoom(8);//Remove this if the link had been added.
+            window.location.href = "dog_list.php"// linked to info view of the dog.
+            map.setZoom(6);//Remove this if the link had been added.
             });
-            google.maps.event.addListener(hmarker, 'click', function() {
-            window.location.href = this.url; {
-
+            hmarker.addListener('click', function() {
             // linked to info view of the shelter.
             map.setZoom(6);//Remove this if the link had been added.
-            };
+            });
+            function CenterControl(controlDiv, map) {
+
+        // Set CSS for the control border.
+            var controlUI = document.createElement('div');
+            controlUI.style.backgroundColor = '#fff';
+            controlUI.style.border = '2px solid #fff';
+            controlUI.style.borderRadius = '3px';
+            controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+            controlUI.style.cursor = 'pointer';
+            controlUI.style.marginBottom = '22px';
+            controlUI.style.textAlign = 'center';
+            controlUI.title = 'Click to recenter the map';
+            controlDiv.appendChild(controlUI);
+
+            // Set CSS for the control interior.
+            var controlText = document.createElement('div');
+            controlText.style.color = 'rgb(25,25,25)';
+            controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+            controlText.style.fontSize = '16px';
+            controlText.style.lineHeight = '38px';
+            controlText.style.paddingLeft = '5px';
+            controlText.style.paddingRight = '5px';
+            controlText.innerHTML = 'Center Map';
+            controlUI.appendChild(controlText);
+
+            // Setup the click event listeners: simply set the map to Chicago.
+            controlUI.addEventListener('click', function() {
+              map.setCenter(pos);
+            });
+
+      }
 
               //pin marker 
       //       setMarkers(map);
