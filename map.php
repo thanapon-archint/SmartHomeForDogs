1a622730ba96a9b4d794598a420dfa35423491f7
Pun <pannathornn@gmail.com>
2018-01-11 16:50:40 +0700

    addlink

--- map.php
+++ map.php
@@ -76,13 +76,9 @@
             icon:homeicons
             });
             marker.addListener('click', function() {
-            window.location.href = "dog_list.php"// linked to info view of the dog.
-            map.setZoom(6);//Remove this if the link had been added.
-            });
+            window.location.href = "dog_detail.php"// linked to info view of the dog.
             hmarker.addListener('click', function() {
-            // linked to info view of the shelter.
-            map.setZoom(6);//Remove this if the link had been added.
-            });
+            window.location.href = "dog_shelterdetail.php"// linked to info view of the shelter
             function CenterControl(controlDiv, map) {
 
         // Set CSS for the control border.
