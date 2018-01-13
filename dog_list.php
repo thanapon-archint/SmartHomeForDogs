c719a5993989a33a6d1939e55410eb6f5ad2a8e8
Thanapon <31815206+ponbung1234@users.noreply.github.com>
2018-01-13 22:49:19 +0700

    dog list

--- dog_list.php
+++ dog_list.php
@@ -44,6 +44,7 @@ if(isset($_POST['submit']))
   $lo1 = $_POST['lo1'];
   $lo2 = $_POST['lo2'];
   $query = "INSERT INTO dogs (lo1, lo2) VALUES ('$lo1', '$lo2')";
+}
 ?>
 
 <br>
@@ -98,66 +99,17 @@ $i++;
 <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
     <h1 class="w3-margin w3-xlarge">
       <!-- Google Map -->
-          <div id="googleMap" class="w3-grayscale" style="width:100%;height:450px;"></div>
-
-          <!-- Footer -->
-          <footer class="w3-center w3-black w3-padding-16">
-            <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-          </footer>
-
-          <!-- Add Google Maps -->
-          <script>
-          function myMap()
-          {
-            myCenter=new google.maps.LatLng(41.878114, -87.629798);
-            var mapOptions= {
-              center:myCenter,
-              zoom:12, scrollwheel: true, draggable: true,
-              mapTypeId:google.maps.MapTypeId.ROADMAP
-            };
-            var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-
-            var marker = new google.maps.Marker({
-              position: myCenter,
-            });
-            marker.setMap(map);
-          }
-          </script>
-          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgVlJ_NecM0OUGWlenJcQRmYJpyb4i5vU&callback=myMap"></script>
-          <!--
-          To use this code on your website, get a free API key from Google.
-          Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
-          -->
-
+          <?php
+          require_once('map.php');
+          ?>
 
       
     </h1>
 </div>
 
-<!-- Footer -->
-<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
-  <div class="w3-xlarge w3-padding-32">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
- </div>
- <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-</footer>
-
-<script>
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-</script>
+<?php
+require_once('footer.php');
+?>
 
 </body>
 </html>
