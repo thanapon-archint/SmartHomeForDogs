9bf4f0e2637f02571b0aec124c01189bdfa2e297
Thanapon Archint <thanapon.archint@gmail.com>
2018-08-31 15:40:47 +0700

    fix title of the index page

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ require_once('Navbar.php');
 
 <!-- Header -->
 <header class="w3-container w3-red w3-center" style="padding:105px 16px">
-  <h1 class="w3-margin w3-jumbo">SMART HOME FOR DOGS</h1>
+  <h1 class="w3-margin w3-jumbo">SMART HOME FOR PETS</h1>
   <p class="w3-xlarge">LINE 2018</p>
   <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top" onclick="javascript:location.href='dog_list.php'">Get Started</button>
 </header>
